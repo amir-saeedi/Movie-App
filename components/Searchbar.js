541423e23364ae497c2238f8fb0a56d568ad4ed4
amir saeedi <89999824+amir-saeedi@users.noreply.github.com>
2023-02-13 14:20:22 +0330

    fix style search bar and create sreach query

--- components/Searchbar.js
+++ components/Searchbar.js
@@ -6,37 +6,72 @@ import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
 import SearchIcon from '@mui/icons-material/Search';
 import DirectionsIcon from '@mui/icons-material/Directions';
-import { AppBar, Toolbar } from '@mui/material';
+import { AppBar, Backdrop, Button, Card, FormHelperText, Input, InputLabel, TextField, Toolbar, Typography } from '@mui/material';
 import { Box, Container } from '@mui/system';
+//
+import { FormControl } from '@mui/material';
+import Link from 'next/link';
 
 export default function Searchbar() {
     return (
-        <AppBar position='static' sx={{ pt: 10 }}>
+        <AppBar position='static' sx={{ pt: { sx: 0, md: 10 } }}>
             <Container maxWidth="xl">
                 <Toolbar sx={{ display: { xs: 'none', md: 'flex' } }} disableGutters>
                     <Box sx={{ flexGrow: 1 }}>
                         <Paper
                             component="form"
+                            onSubmit={(e) => { console.log(e) }}
                             sx={{ p: '2px 4px', display: 'flex', alignItems: 'center', width: 400, mb: 1 }}
                         >
                             <IconButton sx={{ p: '10px' }} aria-label="menu">
                                 <MenuIcon />
                             </IconButton>
-                            <InputBase
-                                sx={{ ml: 1, flex: 1 }}
-                                placeholder="Search"
-                                inputProps={{ 'aria-label': 'search' }}
-                            />
-                            <IconButton type="button" sx={{ p: '10px' }} aria-label="search">
+                            <TextField id="standard-basic" sx={{ mb: 2, ml: 1, flex: 1 }} name="search" label="Standard" variant="standard" />
+                            <IconButton type="submit" sx={{ p: '10px' }} aria-label="search">
                                 <SearchIcon />
                             </IconButton>
                         </Paper>
                     </Box>
                     <Box sx={{ flexGrow: 0 }}>
-                        hello
+                        <Container maxWidth="xl">
+                            <Card sx={{ display: "flex" ,background:"transparent", boxShadow:"none",gap:2}}>
+                                <Typography variant='h6'>
+                                    <Link href={"/hello"}>
+                                        #Best Movie
+                                    </Link>
+                                </Typography>
+                                <Typography variant='h6'>
+                                    <Link href={"/hello"}>
+                                        #Best Movie
+                                    </Link>
+                                </Typography>
+                                <Typography variant='h6'>
+                                    <Link href={"/hello"}>
+                                        #Best Movie
+                                    </Link>
+                                </Typography>
+                            </Card >
+                            <Card sx={{ display: "flex", background: "transparent", boxShadow: "none", gap: 2 }}>
+                                <Typography variant='h6'>
+                                    <Link href={"/hello"}>
+                                        #Best Movie
+                                    </Link>
+                                </Typography>
+                                <Typography variant='h6'>
+                                    <Link href={"/hello"}>
+                                        #Best Movie
+                                    </Link>
+                                </Typography>
+                                <Typography variant='h6'>
+                                    <Link href={"/hello"}>
+                                        #Best Movie
+                                    </Link>
+                                </Typography>
+                            </Card>
+                        </Container>
                     </Box>
                 </Toolbar>
             </Container>
-        </AppBar>
+        </ AppBar>
     );
 }
